529e9682ab6adcad1e8ca4785bcd419464867ff3
Marcin Kass <marcin.kass10@gmail.com>
2016-04-25 19:51:52 +0200

    Game
    Created in Unity

---
+++ Assets/Scripts/Campfire.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Campfire : MonoBehaviour
+{
+    public Text MessageText;
+    public void PointerEnter()
+    {
+        MessageText.text = "Patrzysz na ognisko!";
+    }
+
+    public void PointerExit()
+    {
+        MessageText.text = "";
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Player : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    public CharacterController PlayerChController;
+    public Camera PlayerCamera;
+
+    public float Speed = 5;
+    [SerializeField]
+    private Vector2 RotationSensivity;
+    private Vector3 prevMousePosRel;
+    // Use this for initialization
+    void Start ()
+    {
+        prevMousePosRel = new Vector3(0.5f, 0.5f);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+        RotatePlayer();
+	}
+
+    private void RotatePlayer()
+    {
+        Vector3 mousePosRel = PlayerCamera.ScreenToViewportPoint(Input.mousePosition);
+        if(prevMousePosRel != mousePosRel)
+        {
+            PlayerTransform.Rotate(Vector3.up, (mousePosRel.x - prevMousePosRel.x) * RotationSensivity.x);
+            PlayerCamera.transform.Rotate(Vector3.right, -(mousePosRel.y - prevMousePosRel.y) * RotationSensivity.y);
+            prevMousePosRel = mousePosRel;
+        }
+    }
+
+    private void Move()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if(input != Vector2.zero)
+        {
+            PlayerTransform.Rotate(Vector3.up, input.x);
+            PlayerChController.SimpleMove(PlayerTransform.forward * input.y* Speed);
+        }
+    }
+}
